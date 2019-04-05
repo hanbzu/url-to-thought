6451903fe93d306049fbb54670dc624155718807
Hibai Unzueta <hibai.unzueta@gmail.com>
2019-04-05 23:38:34 +0200

    It's alive

--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import LZUTF8 from "lzutf8";
 import "./index.css";
-
-var parsedUrl = new URL(window.location.href);
-
-console.log(parsedUrl.hash);
 
 function Editor() {
   const [text, setText] = useState("");
   const updateText = text => {
     const compressed = LZUTF8.compress(text, { outputEncoding: "Base64" });
     window.location.hash = "#" + compressed;
+    document.title = `${text} ${text.length > 0 ? " ‒ " : ""}URL to thought`;
     setText(text);
   };
 
+  useEffect(() => {
+    const parsedUrl = new URL(window.location.href);
+    const encoded = parsedUrl.hash.substring(1);
+    const text = LZUTF8.decompress(encoded, { inputEncoding: "Base64" });
+    setText(text);
+    document.title = `${text} ${text.length > 0 ? " ‒ " : ""}URL to thought`;
+  }, []);
+
   return (
     <>
-      <div>{text.length}</div>
       <textarea value={text} onChange={e => updateText(e.target.value)} />
+      <div>{text.length}</div>
     </>
   );
 }
 
 ReactDOM.render(<Editor />, document.getElementById("root"));
-
-window.location.hash = "#doing";
