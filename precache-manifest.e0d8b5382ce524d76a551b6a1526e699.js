3ef9ba556b696d5aec3b192365d5a5ce2acdea5c
Hibai Unzueta <hibai.unzueta@gmail.com>
2019-04-05 23:09:30 +0200

    Updates

---
+++ precache-manifest.e0d8b5382ce524d76a551b6a1526e699.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "929d15fde03df30f0496",
+    "url": "/url-to-thought/static/js/runtime~main.49d88b0e.js"
+  },
+  {
+    "revision": "35899320d24f4de94c66",
+    "url": "/url-to-thought/static/js/main.f7eaada4.chunk.js"
+  },
+  {
+    "revision": "ee61a767bbc71d4d1a61",
+    "url": "/url-to-thought/static/js/2.03332e54.chunk.js"
+  },
+  {
+    "revision": "35899320d24f4de94c66",
+    "url": "/url-to-thought/static/css/main.e9bf1998.chunk.css"
+  },
+  {
+    "revision": "9cc8f66bcca485f92055bc47c6443319",
+    "url": "/url-to-thought/index.html"
+  }
+];
